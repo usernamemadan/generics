821fcd4b4d1ba9d3058bc5810949abb6ca2b9688
madan <madan@local.com>
2021-09-09 00:59:32 +0530

    added code to find maximum of three strings

---
+++ src/com/generics/src/com/generics/Main.java
@@ -0,0 +1,10 @@
+package com.generics;
+
+public class Main {
+	public static void main(String[] args) {
+		Maximum.maximum("a", "b", "c");
+		Maximum.maximum("anil","anirudh","amar");
+		Maximum.maximum("dog", "ant", "cat");
+		
+	}
+}
