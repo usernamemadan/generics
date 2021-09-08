66dfbeacba65438d7741d09f4626995889a210b6
madan <madan@local.com>
2021-09-09 01:07:12 +0530

    added code to find max of 3 strigs

--- src/com/generics/Main.java
+++ src/com/generics/Main.java
@@ -2,13 +2,9 @@ package com.generics;
 
 public class Main {
 	public static void main(String[] args) {
-		Maximum.maximum(4, 6, 7);
-		Maximum.maximum(278, 576, 58);
-		Maximum.maximum(444, 45, 1);
-		
-		MaximumUC2.maximum(4.5f, 6.5f, 7.6f);
-		MaximumUC2.maximum(278.78f, 576.89f, 58.09f);
-		MaximumUC2.maximum(444.7f, 45.5f, 1.1f);
+		Maximum.maximum("a", "b", "c");
+		Maximum.maximum("anil","anirudh","amar");
+		Maximum.maximum("dog", "ant", "cat");
 		
 	}
 }
